60e28fb91c7dee25c3f9815057a14128fae6a9b4
Justin Lawson <jstn.lawson@gmail.com>
2023-07-13 19:20:20 -0500

    started to set up some route stuff

--- server/routes/survey.router.js
+++ server/routes/survey.router.js
@@ -12,6 +12,22 @@ router.get('/', (req, res) => {
     });
 })
 
+//POST route
+router.post('/', (req, res) => {
+    const feedback = req.body;
+    const sqlText = `INSERT INTO feedback (feeling, understanding, support, comments)
+                     VALUES ($1, $2, $3, $4);`;
+    pool.query(sqlText, [feedback.feeling, feedback.understanding, feedback.support, feedback.comments])
+        .then((result) => {
+            console.log(`Added feedback to the database`, gallery);
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); 
+        })
+})
+
 
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom/cjs/react-router-dom.min';
+
 import FeelingForm from './FeelingForm/FeelingForm';
 import UnderstandForm from './UnderstandForm/UnderStandForm';
 import SupportForm from './SupportForm/SupportForm'
+import CommentForm from './CommentForm/CommentForm'
 
 function App() {
 
@@ -11,23 +14,41 @@ function App() {
     event.preventDefault();
 
     axios.post('/api/order',
-        customerToAdd
+      customerToAdd
     ).then(response => {
-        setCustomerToAdd('')
+      setCustomerToAdd('')
     }).catch(err => {
-        console.log('error adding customer info: ', err);
+      console.log('error adding customer info: ', err);
     })
-};
+  };
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
-        <FeelingForm />
-        <UnderstandForm />
-        <SupportForm />
+        
+        
+      
       </header>
+      <Router>
+        <Route exact path="/feeling">
+          <FeelingForm />
+        </Route>
+        <Route exact path="/understand">
+          <UnderstandForm />
+        </Route>
+        <Route exact path="/support">
+          <SupportForm />
+        </Route>
+        <Route exact path="/comment">
+          <CommentForm />
+        </Route>
+        
+        
+        
+      </Router>
+
     </div>
   );
 }
