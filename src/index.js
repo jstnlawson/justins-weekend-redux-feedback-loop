1b5abe8a1c534c63de73e4647c936bad36930357
Justin Lawson <jstn.lawson@gmail.com>
2023-07-13 22:22:41 -0500

    indexJs boiler plate

--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger'
+
+const surveyReducer = (state = [], action) => {
+    if (action.type === 'SET_SURVEY') {
+        return [...action.payload]
+    }else if (action.type === 'EMPTY') {
+        return []
+    }
+    return state
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        surveyReducer
+    }),
+    applyMiddleware(logger)
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <Provider store={storeInstance}>
         <App />
+        </Provider>
     </React.StrictMode>
 );
